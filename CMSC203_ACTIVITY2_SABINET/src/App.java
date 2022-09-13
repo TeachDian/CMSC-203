5f12f7df2bf2b2246216b68faa686e1a1bfe03f5
John Philip Sabinet <93175281+TeachDian@users.noreply.github.com>
2022-09-13 12:01:38 +0800

    Add files via upload

---
+++ CMSC203_ACTIVITY2_SABINET/src/App.java
@@ -0,0 +1,20 @@
+import java.util.jar.Attributes.Name;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String Name =  "John Philip Sabinet";
+        String YOld = "18";
+        String GPA  = "1.25";
+        String BTyp = "O+";
+
+        System.out.println("\nHi, my name is "+Name+".");
+        System.out.println("\nI am "+YOld+" years old.");
+        System.out.println("\nMy GPA is "+GPA+".");
+        System.out.println("\nMy blood type is "+BTyp+".");
+
+// Hi, my name is Regina Castillo.
+// I am 20 years old.
+// My GPA is 2.25.
+// My blood type is B.       
+    }
+}
