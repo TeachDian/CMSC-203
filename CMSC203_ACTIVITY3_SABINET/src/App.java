745776884b9fd89863f618f67ba18cdbc338291d
Philip Gardiola Sabinet <93175281+TeachDian@users.noreply.github.com>
2022-10-19 12:08:23 +0800

    Update App.java

--- CMSC203_ACTIVITY3_SABINET/src/App.java
+++ CMSC203_ACTIVITY3_SABINET/src/App.java
@@ -1,28 +1,60 @@
+//Uber
 import java.util.Scanner;
-public class App {
-    public static void main(String[] args) throws Exception {        
-        Scanner scan = new Scanner(System.in);
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
         boolean done = false;
+        
         do{
-            System.out.print("\nNo. of Test Cases: ");
-            int zx = scan.nextInt();
-            System.out.println("Uber & Grab");
-            if (1 <= zx && zx <= 10) {
-                String inpts[] = new String [zx];
-                for(int i = 0; i < zx; i++){
-                    scan = new Scanner(System.in);
-                    inpts[i] = scan.nextLine();
+            System.out.print("No. of test cases: ");
+            int testCases = scanner.nextInt();
+            
+            if (1 <= testCases && testCases <= 10){
+                String inputs[] = new String[testCases];
+                for (int i = 0; i < testCases; i++) {
+                    scanner = new Scanner(System.in);
+                    inputs[i] = scanner.nextLine();
                 }
-                System.out.println("\nResults: ");
+                
+                System.out.println();
+                System.out.print("Result: ");
                 System.lineSeparator();
-                for(String inpt : inpts){
-                    System.out.println(processInput(inpt));
-                }
-            }
-        } while(done);
-        System.out.println();
-    }
-    private static char[] processInput(String inpt) {
-        return null;
+                for (String input : inputs)
+                    System.out.println(processInput(input));
+                    
+                    
+                done = true;
+            } else
+                System.out.println("Enter numbers 1-10 only!");
+                
+        } while (!done);
+        scanner.close();
     } 
+    
+    private static String processInput(String input) {
+        String[] inputs = input.split("\\s+");
+        
+        if (inputs.length != 2)
+            return "Invalid Input";
+        try {
+            int uber = Integer.parseInt(inputs[0]);
+            int grab = Integer.parseInt(inputs[1]);
+            
+            if (uber < 1 || uber > 1000 
+                    || grab < 1 || grab > 1000)
+                return "Invalid Input";
+                
+            if (uber < grab)
+                return "First";
+            
+            else if (uber > grab)
+                return "Second";
+                
+            else
+                return "Any";
+        } catch (Exception e) {
+            return "Invalid Input";
+        }
+    }    
 }
